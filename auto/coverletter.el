eb2b878957b6b0b3e9b56f0809c730196986bfec
Diego Muñoz Gacitúa <diego.munoz.g@ug.uchile.cl>
2020-08-26 12:52:21 -0400

    5 revisiones enmendadas

--- auto/coverletter.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "coverletter"
- (lambda ()
-   (TeX-run-style-hooks
-    "latex2e"
-    "letter"
-    "letter10"))
- :latex)
-
--- auto/revision_cover_letter.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "revision_cover_letter"
- (lambda ()
-   (TeX-run-style-hooks
-    "latex2e"
-    "letter"
-    "letter10"))
- :latex)
-
